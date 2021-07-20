623f66a3384f2f39009e4ceffe884e1485ba7b03
Austin Huffman <austinhuffman0@gmail.com>
2021-07-20 14:12:52 -0400

    Pseudo Code written for basic layout

---
+++ db/seed.sql
@@ -0,0 +1,49 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+USE employee_db;
+
+
+CREATE TABLE department(
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(30),
+PRIMARY KEY (id)
+);
+
+INSERT INTO department(NAME)
+VALUES("LEADS");
+
+INSERT INTO department(NAME)
+VALUES("Support");
+
+INSERT INTO department(NAME)
+VALUES("Tech Support");
+
+CREATE TABLE role(
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30),
+salary DEC(10,2) NOT NULL,
+department_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+
+INSERT INTO role(title,salary,department_id)
+VALUES("Senior Dev", 111000,1);
+
+
+CREATE TABLE employee(
+id INT NOT NULL AUTO_INCREMENT,
+role_id INT NOT NULL,
+manager_id INT NULL,
+first_name VARCHAR(30) NULL,
+last_name VARCHAR(30) NULL,
+
+FOREIGN KEY (role_id) references role(id),
+FOREIGN KEY (manager_id) references employee (id),
+PRIMARY KEY (id) 
+);
+
+INSERT INTO employee(first_name,last_name,role_id,manager_id)
+VALUES ("Topher","Grace",1,null),("William","Shatner",2,1), ("Ron", "Swanson", 2 , null), ("Leslie","Knope",3,2);
---
+++ employeeDBConnection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const password = require('./password');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    //Use your own
+    password: password,
+    database: 'employee_db',
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log(`Connected to ${connection.threadId}`);
+    connection.end;
+});
